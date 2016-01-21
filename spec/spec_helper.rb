74975ff83026e5725f2d7cf21e8f7f2566886b10
Braden <braden.douglass@gmail.com>
2016-01-21 15:16:48 -0500

    Add Rspec Specific Items To Spec Helper
    Spec helper should have knowledge of the rspec/rails gem. Including
    small setup block for RSpec in general.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'rspec/rails'
 require 'ship_fosdick'
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+end
