58e1751bb9ebccc74de23230772f9511a285bde0
Braden <braden.douglass@gmail.com>
2016-01-21 15:52:05 -0500

    Clean Up If Rails Block
    Assuming this will only work with rails now that this will be an Engine
    over a strait gem.

--- lib/ship_fosdick.rb
+++ lib/ship_fosdick.rb
@@ -1,23 +1,17 @@
+require 'spree_core'
 require 'ship_fosdick/version'
 require 'ship_fosdick/railtie'
 require 'ship_fosdick/engine'
 
 # Fosdick integration entry point.
-# Will check to see if Rails and a confuration is available.
+# Will check to see if Rails confuration is available.
 # Including bootstrapping all needed files for your shipping pleasure
 module ShipFosdick
-  if defined?(Rails)
-    def self.configure(&block)
-      if block_given?
-        block.call(ShipFosdick::Railtie.config.ship_fosdick)
-      else
-        ShipFosdick::Railtie.config.ship_fosdick
-      end
-    end
-  else
-    def self.config
-      @@config ||= OpenStruct.new()
-      @@config
+  def self.configure(&block)
+    if block_given?
+      block.call(ShipFosdick::Railtie.config.ship_fosdick)
+    else
+      ShipFosdick::Railtie.config.ship_fosdick
     end
   end
 end
