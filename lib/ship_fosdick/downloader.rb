233ad84445cc2b04d57b9fa246e41b296ab58c71
Braden <braden.douglass@gmail.com>
2016-01-29 11:58:27 -0500

    Finish MVP Of File Downloader
    File downloader will return an array of content files from the s3
    bucket. These returned files will only be the ones that are uploaded
    from Fosdick. The check on this will have to be tweaked if and when
    Fosdick change their naming scheme.

--- lib/ship_fosdick/downloader.rb
+++ lib/ship_fosdick/downloader.rb
@@ -3,5 +3,19 @@ module ShipFosdick
     def initialize(bucket)
       @bucket = bucket
     end
+
+    def download
+      [].tap do |returned_files|
+        bucket.objects.each do |object|
+          next unless object.key.downcase.include?('shp')
+          returned_files << object.content
+        end
+      end
+    end
+
+    private
+
+    attr_reader :bucket
+
   end
 end
