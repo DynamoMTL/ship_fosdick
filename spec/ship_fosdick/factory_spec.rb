8c627977fefec874d76bde09f35f0650b94fc59c
Braden <braden.douglass@gmail.com>
2016-02-28 15:02:35 -0500

    Factory Pattern For Uploading A Document
    Due to the use of this gem being predominantly through rake tasks, move
    most logic into a single entry point for logic. This is basically the
    factory pattern so incorporate this in the uploading of documents. Old
    shipment upload spec moved to factory spec. Green tests! Document is
    uploaded successfully to s3

---
+++ spec/ship_fosdick/factory_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+require 's3'
+
+RSpec.describe ShipFosdick::Factory, :slow do
+  let!(:order) { create :order_ready_to_ship }
+  let(:service) { S3::Service.new(:access_key_id => ENV.fetch('AWS_ACCESS_KEY_ID'), 
+                                  :secret_access_key => ENV.fetch('AWS_SECRET_ACCESS_KEY')) }
+  let(:bucket) { service.buckets.find(ENV.fetch('S3_BUCKET')) }
+
+  before :each do
+    wipe(bucket)
+  end
+
+  it 'instantiates itself successfully' do
+    expect(described_class.new).to be_truthy
+  end
+
+  describe '#call' do
+    subject { described_class.new.call }
+
+    it 'responds successfully to the message' do
+      expect(subject).to be_truthy
+    end
+
+    it 'successfully uploads the shipment to s3' do
+      subject
+      expect(bucket.objects.count).to eq 1
+      expect(bucket.objects.first.content).to_not be_empty
+    end
+  end
+
+  def wipe(bucket)
+    bucket.objects.each do |object|
+      object.destroy
+    end
+  end
+end
