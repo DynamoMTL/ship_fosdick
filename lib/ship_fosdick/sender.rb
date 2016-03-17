605937de0d8b690a62fa400219b7c57bd73ca052
Peter Berkenbosch <peter@pero-ict.nl>
2016-03-17 17:48:53 +0100

    Add Sender class to push shipment xml files to Fosdick

---
+++ lib/ship_fosdick/sender.rb
@@ -0,0 +1,33 @@
+require 'httparty'
+module ShipFosdick
+  class Sender
+    include HTTParty
+    base_uri 'https://www.unitycart.com'
+    format :xml
+
+    def self.send_doc(doc, config)
+      client = config.fetch(:client_name)
+      res    = post("/#{client}/cart/ipost.asp", body: doc)
+      validate(res)
+      return res['UnitycartOrderResponse']['OrderResponse']
+    end
+
+    private
+    def self.validate(res)
+      order_res = res['UnitycartOrderResponse']['OrderResponse']
+
+      case
+      when order_res.kind_of?(Hash)
+        errors = order_res.has_key?('ErrorCode')
+      when order_res.kind_of?(Array)
+        errors = order_res.first.has_key?('ErrorCode')
+      else
+        errors = nil
+      end
+
+      raise SendError, order_res if errors
+    end
+  end
+
+  class SendError < StandardError; end
+end
--- ship_fosdick.gemspec
+++ ship_fosdick.gemspec
@@ -31,6 +31,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'nokogiri'
   spec.add_dependency 'aws-sdk'
   spec.add_dependency 's3'
+  spec.add_dependency 'httparty'
 
   spec.add_development_dependency 'bundler', '~> 1.11'
   spec.add_development_dependency 'rake', '~> 10.0'
@@ -39,4 +40,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'ffaker'
   spec.add_development_dependency 'sqlite3'
   spec.add_development_dependency 'dotenv-rails'
+  spec.add_development_dependency 'vcr'
+  spec.add_development_dependency 'webmock'
+
 end
