9036973d43647889bcc85e7aa49271e2f23ee7e7
Peter Berkenbosch <peter@pero-ict.nl>
2016-03-31 14:23:30 +0200

    add address wrapping when longer then 30 chars or otherwise specified.

--- spec/ship_fosdick/documents/shipment_spec.rb
+++ spec/ship_fosdick/documents/shipment_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe ShipFosdick::Document::Shipment do
   let(:order) { create :order_with_line_items }
   let(:shipment) { order.shipments.first }
   let(:long_city) { 'InFactThisIsMoreThanTwelveCharacters' }
+  let(:long_address1) { 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }
 
   it 'instantiates itself' do
     expect(described_class.new(shipment)).to be_truthy
@@ -52,6 +53,14 @@ RSpec.describe ShipFosdick::Document::Shipment do
       it 'returns a ShipStateOrder node for countries with states outside of the US' do
         order.shipping_address.country.update_attribute(:iso3, 'CAN')
         expect(subject).to include 'ShipStateOther'
+      end
+
+      it 'wraps a long address1 into address2' do
+        order.shipping_address.update_attribute(:address1, long_address1)
+        line1_xml = "<ShipAddress1>Lorem ipsum dolor sit amet, co</ShipAddress1>"
+        line2_xml = "<ShipAddress2>nsectetur adipiscing elit., No</ShipAddress2>"
+        expect(subject).to include line1_xml
+        expect(subject).to include line2_xml
       end
     end
 
