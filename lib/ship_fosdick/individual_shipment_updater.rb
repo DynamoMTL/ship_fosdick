33265a804c4774558d59fe870000ecfbc8ef8c8d
Daniel Wright <daniel@godynamo.com>
2016-07-14 14:28:43 -0400

    A Failed Update Raises an Exception
    When the IndividualShipmentUpdater cannot update a shipment, instead of failing
    silently, it will now raise a ShipFosdick::FailedUpdateError. Additionally, the
    update is performed atomically, so shipments should remain in a reliable state
    even on failures.

--- lib/ship_fosdick/individual_shipment_updater.rb
+++ lib/ship_fosdick/individual_shipment_updater.rb
@@ -1,50 +1,55 @@
 module ShipFosdick
   class IndividualShipmentUpdater
-    class InvalidTransitionError < StandardError; end
     class NoOrderError < RuntimeError; end
+    class FailedUpdateError < StandardError; end
 
     def initialize(manifest_row,
                    shipment_repository: Spree::Shipment,
-                   invalid_transition_error_factory: InvalidTransitionError,
+                   failed_update_error_factory: FailedUpdateError,
                    no_order_error_factory: NoOrderError)
 
       @manifest_row = manifest_row
       @shipment_repository = shipment_repository
-      @invalid_transition_error_factory = invalid_transition_error_factory
+      @failed_update_error_factory = failed_update_error_factory
       @no_order_error_factory = no_order_error_factory
     end
 
     def perform
       return unless can_update?
 
-      raise_invalid_transition_error unless shipment.can_ship?
       raise_no_order_error unless shipment.order
 
-      update_attributes
-      transition_state
-      update_order
+      result = atomically do
+        update_attributes and
+          transition_state and
+          update_order
+      end || raise_failed_update_error
     end
 
     alias_method :update, :perform
 
     private
 
-    attr_reader :invalid_transition_error_factory, :manifest_row,
+    attr_reader :failed_update_error_factory, :manifest_row,
       :no_order_error_factory, :shipment_repository
+
+    def atomically(&block)
+      ActiveRecord::Base.transaction(&block)
+    end
 
     def can_update?
       shipment &&
         !shipment.shipped?
     end
 
-    def raise_invalid_transition_error
-      raise invalid_transition_error_factory,
-        "Cannot transition shipment from current state: '#{shipment.state}' to requested state: 'shipped', no transition found."
-    end
-
     def raise_no_order_error
       raise no_order_error_factory,
         "No order associated with #{manifest_row.shipment_number}, which should be impossible."
+    end
+
+    def raise_failed_update_error
+      raise failed_update_error_factory,
+        "Updating shipment #{manifest_row.shipment_number} failed."
     end
 
     def shipment
--- spec/ship_fosdick/individual_shipment_updater_spec.rb
+++ spec/ship_fosdick/individual_shipment_updater_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe ShipFosdick::IndividualShipmentUpdater do
   class MockException < StandardError; end
 
   let(:shipment_repository) { double(:shipment_repository) }
-  let(:invalid_transition_error_factory) { MockException }
+  let(:failed_update_error_factory) { MockException }
   let(:no_order_error_factory) { MockException }
 
   subject(:updater) do
     described_class.new(manifest_row,
                         shipment_repository: shipment_repository,
-                        invalid_transition_error_factory: invalid_transition_error_factory,
+                        failed_update_error_factory: failed_update_error_factory,
                         no_order_error_factory: no_order_error_factory)
   end
 
@@ -19,6 +19,7 @@ RSpec.describe ShipFosdick::IndividualShipmentUpdater do
   let(:shipment) { double(:shipment, order: order) }
 
   before do
+    allow(ActiveRecord::Base).to receive(:transaction).and_yield.and_return(true)
     allow(shipment_repository).to receive(:find_by).and_return(shipment)
   end
 
@@ -28,27 +29,31 @@ RSpec.describe ShipFosdick::IndividualShipmentUpdater do
 
       allow(shipment).to receive(:update) { true }
       allow(shipment).to receive(:shipped?) { false }
-      allow(shipment).to receive(:can_ship?) { true }
       allow(shipment).to receive(:ship) { true }
-      allow(shipment).to receive(:state) { 'ready' }
     end
 
-    it "updates the shipment's 'tracking' attribute" do
-      expect(shipment).to receive(:update).with(tracking: 'ABCD1234')
+    context "atomically" do
+      before do
+        expect(ActiveRecord::Base).to receive(:transaction)
+      end
 
-      perform!
-    end
+      it "updates the shipment's 'tracking' attribute" do
+        expect(shipment).to receive(:update).with(tracking: 'ABCD1234')
 
-    it "transitions the shipment to the 'shipped' state" do
-      expect(shipment).to receive(:ship)
+        perform!
+      end
 
-      perform!
-    end
+      it "transitions the shipment to the 'shipped' state" do
+        expect(shipment).to receive(:ship)
 
-    it "updates the shipment's order" do
-      expect(order).to receive(:update!)
+        perform!
+      end
 
-      perform!
+      it "updates the shipment's order" do
+        expect(order).to receive(:update!)
+
+        perform!
+      end
     end
 
     context "when a matching shipment cannot be found" do
@@ -71,18 +76,6 @@ RSpec.describe ShipFosdick::IndividualShipmentUpdater do
       end
     end
 
-    context "when the shipment cannot be transitioned to the 'shipped' state" do
-      before do
-        allow(shipment).to receive(:can_ship?).and_return(false)
-      end
-
-      it "raises an exception" do
-        cause = ->{ perform! }
-
-        expect(&cause).to raise_error(MockException)
-      end
-    end
-
     context "when the shipment has no order" do
       before do
         allow(shipment).to receive(:order).and_return(nil)
@@ -92,6 +85,48 @@ RSpec.describe ShipFosdick::IndividualShipmentUpdater do
         cause = ->{ perform! }
 
         expect(&cause).to raise_error(MockException)
+      end
+    end
+
+    context "when the shipment cannot be updated" do
+      before do
+        allow(ActiveRecord::Base).to receive(:transaction).and_yield.and_return(false)
+      end
+
+      context "with a failed attribute update" do
+        before do
+          allow(shipment).to receive(:update).and_return(false)
+        end
+
+        it "raises an exception" do
+          cause = ->{ perform! }
+
+          expect(&cause).to raise_error(MockException)
+        end
+      end
+
+      context "when the shipment cannot be transitioned to the 'shipped' state" do
+        before do
+          allow(shipment).to receive(:ship).and_return(false)
+        end
+
+        it "raises an exception" do
+          cause = ->{ perform! }
+
+          expect(&cause).to raise_error(MockException)
+        end
+      end
+
+      context "when the order fails to update" do
+        before do
+          allow(order).to receive(:update!).and_return(false)
+        end
+
+        it "raises an exception" do
+          cause = ->{ perform! }
+
+          expect(&cause).to raise_error(MockException)
+        end
       end
     end
 
